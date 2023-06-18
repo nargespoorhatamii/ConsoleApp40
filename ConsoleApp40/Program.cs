d3cdf664ef0fdec2e2d0dae756fa16e25103846c
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-06-18 12:42:16 +0430

    Add project files.

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp40
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("inter number1:");
+            int  num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inter number2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            double sum = num1 + num2;
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
